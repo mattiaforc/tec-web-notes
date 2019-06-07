f2507dc897f0d2622aec93c76df3cac0f083d56e
MalattiaForcellese <mattiaforc@gmail.com>
2019-06-07 15:48:24 +0200

    Exercise 2 SAXParser working

--- Exercises/02_TecWeb/src/parserSAX/Main.java
+++ Exercises/02_TecWeb/src/parserSAX/Main.java
@@ -4,18 +4,38 @@ import java.time.LocalDate;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.Attributes;
+import org.xml.sax.ErrorHandler;
 
 
-public class main {
+public class Main {
 	private enum fields {lettera, mittente, data, destinatario, saluto, corpo, paragrafo, chiusura, firma}
 	
-	private static class letteraHandler extends DefaultHandler {
+	private static class ErrorsHandler implements ErrorHandler {		
+		@Override
+		public void warning (SAXParseException e) { System.out.println("Parsing Warning: "+e.getMessage()); }
+		
+		@Override
+		public void error (SAXParseException e) {
+			System.out.println("XML document NOT valid");
+			System.out.println("Parsing Error: "+e.getMessage());
+		}
+		
+		@Override
+		public void fatalError (SAXParseException e) {
+			System.out.println("XML document NOT well-formed");
+			System.out.println("Parsing Fatal Error: "+e.getMessage());
+		}
+	}
+	
+	private static class LetteraHandler extends DefaultHandler {
 		private String mittente, destinatario, saluto, chiusura, firma, paragrafo;
 		private LocalDate data;
 		private boolean[] inElement = new boolean[fields.values().length];
 		
-		public void startElement (String namespaceURI, String localName, String rawName) {
+		public void startElement (String namespaceURI, String localName, String rawName, Attributes atts) {
 			for(var e : fields.values()) {
 				if(localName.equals(e.name())) inElement[e.ordinal()] = true;
 			}
@@ -35,8 +55,16 @@ public class main {
 			for(var e : fields.values()) {
 				if(localName.equals(e.name())) inElement[e.ordinal()] = false;
 			}
-			if (localName.equals("lettera"))
-				System.out.println("[LETTERA]: " + mittente + "; " + destinatario + "; " + saluto + "; " + paragrafo + "; " + chiusura + "; " + firma);
+		}
+
+		public String getResultAsString() {
+			return "Mittente: " + mittente + ";\t" + 
+					"Destinatario: " + destinatario + ";\t" +
+					"Data: " + data.toString() + ";\t" +
+					"Saluto: " + saluto + ";\t" + 
+					"Paragrafo: " + paragrafo + ";\t" + 
+					"Chiusura: " + chiusura + ";\t" + 
+					"Firma: " + firma;
 		}
 	}
 	
@@ -44,11 +72,18 @@ public class main {
 		try {
 			SAXParserFactory SAXPFactory = SAXParserFactory.newInstance();
 			SAXPFactory.setValidating(true);
+			SAXPFactory.setNamespaceAware(true);
 			var parser = SAXPFactory.newSAXParser();
 			var reader = parser.getXMLReader();
-			reader.setContentHandler(new letteraHandler());
+			String schemaFeature = "http://apache.org/xml/features/validation/schema";
+			String ignorableWhitespace = "http://apache.org/xml/features/dom/include-ignorable-whitespace";
+			var handler = new LetteraHandler();
+			reader.setContentHandler(handler);
+			reader.setErrorHandler(new ErrorsHandler());
+			reader.setFeature(schemaFeature, true);
 			for(var arg : args) {
 				reader.parse(arg);
+				System.out.println(handler.getResultAsString());
 			}
 		}
 		catch (Exception e) {
